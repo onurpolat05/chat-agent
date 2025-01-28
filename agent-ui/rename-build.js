3917cb28c5a1fb3b61c050864fa2978241f3b848
Onur Polat <onurpolat711@gmail.com>
2025-01-29 02:46:41 +0300

    fix

--- agent-ui/rename-build.js
+++ agent-ui/rename-build.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const buildPath = path.join(__dirname, 'build', 'static', 'js');
+
 const files = fs.readdirSync(buildPath);
 
 files.forEach(file => {
@@ -9,10 +10,15 @@ files.forEach(file => {
         //fs.renameSync(path.join(buildPath, file), path.join(buildPath, 'main.js'));
         fs.copyFileSync(path.join(buildPath, file), path.join(buildPath, 'main.js'));
     }
-    // css files
+});
+
+const buildCssPath = path.join(__dirname, 'build', 'static', 'css');
+
+const cssFiles = fs.readdirSync(buildCssPath);
+
+cssFiles.forEach(file => {
     if (file.startsWith('main.') && file.endsWith('.css')) {
-        //fs.renameSync(path.join(buildPath, file), path.join(buildPath, 'main.css'));
-        fs.copyFileSync(path.join(buildPath, file), path.join(buildPath, 'main.css'));
+        fs.copyFileSync(path.join(buildCssPath, file), path.join(buildCssPath, 'main.css'));
     }
 });
 
